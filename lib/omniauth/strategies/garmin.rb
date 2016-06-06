4a18cc03a47d3070ae437c6f5a5e9d34ad08dc17
craig <cbenerofe@yahoo.com>
2016-06-05 20:15:50 -0400

    back to not test

--- lib/omniauth/strategies/garmin.rb
+++ lib/omniauth/strategies/garmin.rb
@@ -9,10 +9,10 @@ module OmniAuth
 
       option :client_options, {
         scheme: :header,
-        site: 'http://connectapitest.garmin.com',
+        site: 'http://connectapi.garmin.com',
         request_token_path: '/oauth-service-1.0/oauth/request_token',
         access_token_path: '/oauth-service-1.0/oauth/access_token',
-        authorize_url: 'http://connecttest.garmin.com/oauthConfirm'
+        authorize_url: 'http://connect.garmin.com/oauthConfirm'
       }
 
       uid do
